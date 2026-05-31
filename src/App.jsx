f95f2dc174c8b3fbf9bcaad36509eade64beb692
darshanaper111-source <darshana.per111@gmail.com>
2026-06-01 01:06:41 +0530

    testt

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function Header({ active }) {
         {/* Logo */}
         <button onClick={() => nav("home")} className="text-left">
           <div className="text-xl font-black tracking-wide">
-            CEYLON <span className="text-ecoGold">ECO POWER</span>
+            CEYLONTTT <span className="text-ecoGold">ECO POWER</span>
           </div>
           <div className="text-[10px] tracking-[0.3em] text-white/50 uppercase">Solar Engineering</div>
         </button>
